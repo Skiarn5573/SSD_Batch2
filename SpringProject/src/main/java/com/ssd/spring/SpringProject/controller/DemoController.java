8baf4e6844c82ea1892cb0ea5535854c380a110d
somarajukiran <kirangithub28@gmail.com>
2023-02-21 17:01:18 +0530

    Created Controller class

---
+++ SpringProject/src/main/java/com/ssd/spring/SpringProject/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.ssd.spring.SpringProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+
+	
+	
+	@GetMapping("/")
+	public String display() {
+		
+		return "Welcome to world";
+	}
+	
+}
